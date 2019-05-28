0460a3e62ca53c96089e573a57494a93752f2c8d
Andrew English <aje299@gmail.com>
2019-05-27 22:47:57 -0700

    Initial test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "lib/TicTacToe.h"
+#include "lib/NeuralNet.h"
+#include "lib/QLearning.h"
+#include <signal.h>
+
+using std::cout;
 
 int main(int argc, char *argv[])
 {
-    TicTacToe game;
+    Network net(27, 1, 243, 9, relu, linear);
 
-    game.run();
-    system("pause");
+    QLearning::run(net);
+    net.save("Saves/NetworkSave.data");
 
     return 0;
 }
