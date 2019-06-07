669a87b20dd6feb2323bfcb130d082f15d36c15b
Andrew English <aje299@gmail.com>
2019-06-06 21:21:21 -0700

    Player class

---
+++ lib/Player.h
@@ -0,0 +1,62 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "./NeuralNet.h"
+#include "./TicTacToe.h"
+
+//Defines a transition, used in experience replay for learning
+struct Transition
+{
+    vector<double> state;
+    int action;
+    int reward;
+    vector<double> qvals;
+    vector<double> nextState;
+    double nextMax = 0;
+};
+
+// player class, using a network for moves
+class NNPlayer {
+public:
+    NNPlayer(Network *net, bool training = false, int minibatchSize = 16, int replaySize = 128); // constructor
+    ~NNPlayer(); // destructor
+
+    void move(TicTacToe &game); // routes move based on training
+    void final_reward(int outcome); //writes the reward of the move which led to the game outcome (1 is win, -1 is loss, 0 is draw)
+    vector<double> get_qvals(TicTacToe &game); // see the output of the network for testing 
+
+private:
+    Network *net;
+    bool training;
+
+    int rewardWin, rewardLoss, rewardValidMove, rewardDraw;
+    double eps, epsMin; // for e-greedy action selection
+    int minibatchSize, replaySize;
+    int moves; // number of total moves the network has made
+    double gamma; // discount rate of future rewards
+    vector<Transition> replays, miniBatch; // for experience replay and sampling of replays
+
+    //private functions:
+    vector<double> get_input(TicTacToe &game); // changes game board into input for the network
+
+    //returns qvals, but invalid moves are set to a small num. Also populates the possible moves
+    vector<double> get_probs(TicTacToe &game, vector<double> probs, vector<int> &possibleMoves);
+    int argmax(vector<double> const &x); // returns the position of the largest number in the vector
+    int execute_move(TicTacToe &game, int action); // makes a move and returns the reward of the move
+    void network_fit(vector<Transition> const &miniBatch); // updates the network based on a minibatch of transitions
+    void train_move(TicTacToe &game); // Network move for training
+    void notrain_move(TicTacToe &game); // Network move for normal game
+
+};
+
+
+//player class making random moves
+class RANDPlayer {
+public:
+    void move(TicTacToe &game); // makes a move at random based on available moves
+
+private:
+    vector<double> get_moves(TicTacToe &game);
+};
+
+#endif
---
+++ src/Player.cpp
@@ -0,0 +1,200 @@
+#include "../lib/Player.h"
+
+//NNPlayer functions:
+
+NNPlayer::NNPlayer(Network *net, bool training, int minibatchSize, int replaySize){
+    //init all vars
+    this->training = training;
+    this->minibatchSize = minibatchSize;
+    this->replaySize = replaySize;
+    
+    this->moves = 0;
+    this->eps = 1.0;
+    this->epsMin = .05;
+    this->gamma = .9;
+    this->rewardWin = 50;
+    this->rewardLoss = -40;
+    this->rewardValidMove = 5;
+    this->rewardDraw = 0;
+    this->miniBatch = vector<Transition>(minibatchSize);
+    this->net = net;
+}
+
+NNPlayer::~NNPlayer(){} // no mem manually allocated -> no mem to manually unallocate
+
+//routes to the proper move
+void NNPlayer::move(TicTacToe &game)
+{ training ? train_move(game) : notrain_move(game); }
+
+//Gives a normalized network input for a given game
+// First 9 are position of X's, second 9 are positions of O's, and last 9 are position of blanks
+vector<double> NNPlayer::get_input(TicTacToe &game)
+{
+    vector<int> board = game.getBoard();
+    vector<double> input(27, 0);
+
+    for (int i = 0; i < 9; i++){
+        switch (board[i]){
+        case 0:
+            input[9 + i] = 1;
+            break;
+        case 1:
+            input[i] = 1;
+            break;
+        case 2:
+            input[18 + i] = 1;
+            break;
+        default:
+            break;
+        }
+    }
+
+    return input;
+}
+
+//returns the probs and updates possible moves (same loop for both)
+vector<double> NNPlayer::get_probs(TicTacToe &game, vector<double> probs, vector<int> &possibleMoves){
+    vector<int> board = game.getBoard();
+    
+    for(int i = 0; i < board.size(); i++){
+        if(board[i] != 2)
+            probs[i] = -5000; // sets prob of invalid move to something small so we do not select invalid moves 
+        else
+            possibleMoves.push_back(i);
+    }
+
+    return probs;
+}
+
+//returns the position of the largest value in the vector
+int NNPlayer::argmax(vector<double> const &x){
+    double max = INT64_MIN;
+    int pos = 0;
+    
+    for(int i = 0; i < x.size(); i++){
+        if (x[i] > max){
+            max = x[i];
+            pos = i;
+        }
+    }
+
+    return pos;
+}
+
+// makes a move and returns the reward for that move
+int NNPlayer::execute_move(TicTacToe &game, int action){
+    int reward;
+
+    if(game.makeMove(action + 1)){
+        reward = this->rewardValidMove;
+    }
+
+    return reward;
+}
+
+void NNPlayer::network_fit(vector<Transition> const &miniBatch){
+    vector<vector<double>> input, tout;
+    vector<double> target;
+
+    for (int i = 0; i < miniBatch.size(); i++){
+        Transition t = miniBatch[i];
+        input.push_back(t.state);
+
+        target = t.qvals;
+        target[t.action] = t.reward == this->rewardWin ? t.reward : t.reward + this->gamma * t.nextMax; // terminal state if reward is rewardWin
+        tout.push_back(target);
+    }
+
+    net->batch_fit(input, tout);
+}
+
+void NNPlayer::train_move(TicTacToe &game){
+    int action, reward;
+    vector<double> state, newState, qvals, probs;
+    vector<int> possibleMoves;
+
+    state = get_input(game);
+    qvals = this->net->run(state);
+    probs = get_probs(game, qvals, possibleMoves); // favors qvals of valid moves to not waste time training on invalid moves
+
+    //Select action using e-greedy:
+    if ((double)rand() / (double)RAND_MAX < eps)
+        action = possibleMoves[rand() % possibleMoves.size()]; // picks random move from possible moves
+    else
+        action = argmax(probs);
+
+    //Take action, observe reward and new state:
+    reward = execute_move(game, action);
+    newState = get_input(game);
+
+    //Change last move's next max to the current max of possible moves
+    if(replays.size() > 0)
+        this->replays[(moves - 1) % replaySize].nextMax = qvals[action];
+
+    //Store new transition in replay:
+    Transition t = {state, action, reward, qvals, newState};
+    if (replays.size() < replaySize)
+        this->replays.push_back(t);
+    else { // replays is full, so rewrite old ones
+        this->replays[moves % replaySize] = t;
+    }
+
+    //Sample mini batch of transitions from replays, then fit to the network:
+    if (this->replays.size() >= this->minibatchSize)
+    {
+        for (int k = 0; k < minibatchSize; k++)
+            this->miniBatch[k] = this->replays[rand() % replays.size()];
+
+        network_fit(miniBatch);
+    }
+
+    this->moves++;
+    if (this->eps > this->epsMin) // diminishes to epsMin over moves
+        eps -= (1 - epsMin) / 5000;
+}
+
+//picks the move with the highest qval based on the state
+void NNPlayer::notrain_move(TicTacToe &game){
+    game.makeMove(argmax(this->net->run(get_input(game))) + 1);
+}
+
+void NNPlayer::final_reward(int outcome){
+    switch(outcome){
+        case 1:
+            replays[(moves - 1) % replaySize].reward = this->rewardWin;
+            break;
+        case 0:
+            replays[(moves - 1) % replaySize].reward = this->rewardWin;
+            break;
+        case -1:
+            replays[(moves - 1) % replaySize].reward = this->rewardWin;
+            break;
+        default:
+            break;
+    }
+}
+
+vector<double> NNPlayer::get_qvals(TicTacToe &game){
+    return net->run(get_input(game));
+}
+
+
+//RANDPlayer functions
+
+void RANDPlayer::move(TicTacToe &game){    
+    vector<double> possibleMoves = get_moves(game);
+    game.makeMove(possibleMoves[rand() % possibleMoves.size()] + 1); // makes a random move
+}
+
+vector<double> RANDPlayer::get_moves(TicTacToe &game){
+    vector<int> board = game.getBoard();
+    vector<double> possibleMoves;
+
+    for(int i = 0; i < board.size(); i++){
+        if(board[i] == 2)
+            possibleMoves.push_back(i);
+    }
+
+    return possibleMoves;
+}
+
