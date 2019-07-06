eb4aef700601305177b8c571cc125986f59db976
drew-english <aje299@gmail.com>
2019-07-06 13:47:02 -0700

    Cleaned up

--- main.cpp
+++ main.cpp
@@ -9,40 +9,7 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL)); // initializes random seed
 
-    Network net(27, 1, 243, 9, relu, linear);
-    NNPlayer netPlayer(&net, true);
-    MINMAXPlayer mmp;
-
-    QLearning::run(netPlayer, &mmp);
-    net.save("Saves/NetworkSave.data");
-
-    // Network Testing:
-    // vector<double> input = {1, 1, 1, 1};
-    // vector<double> tout = {5, 5, 5}, out;
-    // vector<vector <double>> inputs(8), touts(8);
-    // Network testNet(4, 1, 10, 3, relu, linear);
-
-    // out = testNet.run(input);
-    // for(int i = 0; i < 3; i++)
-    //     cout << out[i] << endl;
-
-    // for(int i = 0; i < 2064; i++){
-    //     inputs[i % 8] = input;
-    //     touts[i % 8] = tout;
-    //     if((i + 1) % 8 == 0)
-    //         testNet.batch_fit(inputs, touts);
-    // }
-
-    // cout << endl;
-
-    // out = testNet.run(input);
-    // for(int i = 0; i < 3; i++)
-    //     cout << out[i] << endl;
-
-    // Network net("Saves/NetworkSave.data");
-    // NNPlayer netPlayer(&net);
-
-    // QLearning::playGame(netPlayer);
+    QLearning::run_test();
 
     return 0;
 }
