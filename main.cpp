53f66fb1f3b22aaf18ea50deb18c5f6b63150082
drew-english <aje299@gmail.com>
2019-07-06 21:28:01 -0700

    Plays vs. BestNet

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL)); // initializes random seed
 
-    Network net(27, 1, 243, 9, relu, linear);
-    NNPlayer netPlayer(&net, true);
-    MINMAXPlayer mmp;
-    
-    QLearning::run_test(&mmp, &netPlayer, "testdata/MMP-NET(1).data");
+    // loads bestnet and plays a game
+    Network net("Saves/BestNet.data");
+    NNPlayer netPlayer(&net);
+    QLearning::playGame(&netPlayer);
+
     return 0;
 }
